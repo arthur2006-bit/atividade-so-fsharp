bff70d804068e2534590a7f38aa021d708a89b06
athr santoss <aquinoarthur677@gmail.com>
2026-09-08 14:29:22 -0300

    Update Program.fs

--- Thread1/Program.fs
+++ Thread1/Program.fs
@@ -9,13 +9,13 @@ let minhaFuncao () =
 [<EntryPoint>]
 let main argv =
 
-    // Criar a thread
+    
     let thread = new Thread(ThreadStart(minhaFuncao))
 
-    // Iniciar a thread
+    
     thread.Start()
 
-    // Aguardar a thread terminar
+    
     thread.Join()
 
     printfn "Programa principal finalizado!"
